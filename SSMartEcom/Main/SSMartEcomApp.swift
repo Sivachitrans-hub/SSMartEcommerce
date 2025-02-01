638944e5c3206849bbcf71589f8aa5f5235dc84c
Siva's GitHub <sivamsiva739@gmail.com>
2025-02-01 20:27:14 +0530

    initial and Folder  Structure Created

---
+++ SSMartEcom/Main/SSMartEcomApp.swift
@@ -0,0 +1,20 @@
+//
+//  SSMartEcomApp.swift
+//  SSMartEcom
+//
+//  Created by siva chitran p on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SSMartEcomApp: App {
+   
+
+    var body: some Scene {
+        WindowGroup {
+            ProductsHomeView()
+               
+        }
+    }
+}
---
+++ SSMartEcom/View/ProductsHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsHomeView.swift
+//  SSMartEcom
+//
+//  Created by siva chitran p on 01/02/25.
+//
+
+import SwiftUI
+
+struct ProductsHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductsHomeView()
+}
